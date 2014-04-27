dd59b92c66d45b5acc52a8b626483ba7396107b5
Cesar Richard <cesar.richard2@gmail.com>
2014-04-28 00:07:57 +0200

    Optimisation du chargement des photos (1 par 1 jusqu'à avoir rempli l'écran)
    Signed-off-by: Cesar Richard <cesar.richard2@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+var actualPage = 1;
+var totalPages = 1;
+var actualAlbumId = 0;
+
 $(function(){
-	$("#photoHD").css('max-height', .90*$( window ).height());
-	$("#photoHD").css('max-width', .90*$( window ).width());	
 
 	getAlbums();
 	
 	dispPublicPhotos();
-
+		
+	
 	$.ajaxSetup({
 		"error":function() { $("#content").append("Such problems");  }
 	});
@@ -14,35 +17,54 @@ $(function(){
 	
 });
 
+function getPhotoAPIUrl(albumId, page){
+	return "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=acf8e0e35f7d224d8f071714e46a851d&photoset_id="+albumId+"&per_page=1&page="+page+"&format=json&nojsoncallback=1";
+}
 
+function initWaypoints(){
+	$.waypoints('destroy');
+	var $loading = $("<div class='loading'><H1><p>Loading more items&hellip;</p></H1></div>");
+    $footer = $('footer');
+    opts = {
+        offset: 'bottom-in-view'
+    };
 
+    $footer.waypoint(function(event, direction) {
+		if(actualPage < totalPages){
+			$.waypoints('disable');
+			$('body').append($loading);
+			$.getJSON(getPhotoAPIUrl(actualAlbumId, actualPage+1), function(data){
+				actualPage=parseInt(data.photoset.page);
+				$("#content").append("<div id='page"+actualPage+"' style='display:none;'>");
+				$.each(data.photoset.photo,function( index, photo) {
+					$("#page"+actualPage).append("<div class='imgContainer'><img photoPage='"+actualPage+"' photoId='"+photo.id+"' src='"+constructPhotoUrl(photo.id,photo.farm,photo.server,photo.secret)+"'/></div>");
+				});
+				$("#content").append("</div>");
+				$("#page"+actualPage).fadeIn( "slow" );
+				$('.loading').remove();
+				$.waypoints('enable');
+				$footer.waypoint(opts);
+			});
+		}
+    }, opts);
+}		
 
-			
+function constructPhotoUrl(id, farm, server, secret){
+	return "http://c2.staticflickr.com/" + farm + "/" + server + "/" + id + "_" + secret + ".jpg";
+}
+
 function getPhotosFromAlbum(albumId){
-	$.getJSON("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=acf8e0e35f7d224d8f071714e46a851d&photoset_id="+albumId+"&per_page=8&format=json&nojsoncallback=1",
-	function(data){
+	actualAlbumId=albumId;
+	
+	$.getJSON(getPhotoAPIUrl(albumId,1), function(data){
 		$("#content").empty();			
 		$("#SetTitle").text(data.photoset.title);
-		$("#content").append("<div page=1>");
+		totalPages = parseInt(data.photoset.pages);		
 		$.each(data.photoset.photo,function( index, photo ) {
-			$("#content").append("<img height='333px' photoPage='1' photoId='"+photo.id+"' src='" + "http://c2.staticflickr.com/" + photo.farm + "/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg'/>");
+			$("#content").append("<div id='page1'><div class='imgContainer'><img photoPage='1' photoId='"+photo.id+"' src='"+constructPhotoUrl(photo.id,photo.farm,photo.server,photo.secret)+"'/></div></div>");
 		});
-		$("#content").append("</div>");
-		if(parseInt(data.photoset.pages) > 1){
-			for (var i = 2; i <= parseInt(data.photoset.pages); i++){
-				$.getJSON("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=acf8e0e35f7d224d8f071714e46a851d&photoset_id="+albumId+"&per_page=8&page="+i+"&format=json&nojsoncallback=1",
-					function(data){
-					page=data.photoset.page;
-					$("#content").append("<div id='page"+page+"' style='display: none;'>");
-					$.each(data.photoset.photo,function( index, photo) {
-						$("#page"+page).append("<img height='333px' photoPage='"+page+"' photoId='"+photo.id+"' src='" + "http://c3.staticflickr.com/" + photo.farm + "/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg'/>");
-					});
-					$("#content").append("</div>");
-				});
-				
-			}
-		}
-		initializeUI();
+		initWaypoints();
+		repaintMenu();
 	});
 }
 
@@ -53,69 +75,61 @@ function getAlbums(){
 		$.each(data.photosets.photoset,function( index, album ) {
 			$("#navigationMenu").append("<li><a albumId='" + album.id + "' href=\"#\" class=\"normalMenu\">" + album.title._content + "</a></li>");
 		});
-		initializeUI();
-		//$("#navigationMenu").prepend("<li><a id="home" href=\"#\" class=\"selectedMenu\">Accueil</a></li>").click(dispPublicPhotos());
+		repaintMenu();
 	});
 }
 
 function initializeUI(){
+	$( document ).on("click", "#content img", function(event){
+			event.preventDefault();
+			PreviewImage($(this).attr('photoId'));
+	});
+	
+	$( document ).on('click', '#navigationMenu li', function(){
+		$(".selectedMenu").each(function(){
+					$(this).removeClass().addClass("normalMenu");
+			$("#navigationMenu li a[albumId="+id+"]").before($(this).clone().removeClass().addClass('hoverMenu'));
+			});
+		
+		id = $(this).children().attr("albumId");
+		getPhotosFromAlbum(id);
+
+		$("#navigationMenu li a[albumId=\""+id+"\"].hoverMenu").remove();
+		$("#navigationMenu li a[albumId="+id+"]").removeClass().addClass("selectedMenu");
+	});
+	
+	$( document ).on('mouseenter', '#navigationMenu li', function(){
+		$(this).find('.hoverMenu').stop().animate({marginTop:'0px'},200);
+	});
+	
+	$( document ).on('mouseleave', '#navigationMenu li', function(){
+		$(this).find('.hoverMenu').stop().animate({marginTop:'-25px'},200);
+	});
+	
+	$("#photoHD").css('max-height', .90*$( window ).height()).css('max-width', .90*$( window ).width());
+	
+	repaintMenu();
+}
+
+function repaintMenu(){
 	$(".selectedMenu").each(function(){
 		$(this).removeClass().addClass("normalMenu").before($(this).children().clone().removeClass().addClass('hoverMenu'));
         });0
 
-
 	$('#navigationMenu li .normalMenu').each(function(){
 		$(this).before($(this).clone().removeClass().addClass('hoverMenu'));
-	});
-	
-	$('#navigationMenu li').each(function(){
-		$(this).click(function(){
-			$(".selectedMenu").each(function(){
-                		$(this).removeClass().addClass("normalMenu");
-				$("#navigationMenu li a[albumId="+id+"]").before($(this).clone().removeClass().addClass('hoverMenu'));
-		        });
-			
-			id = $(this).children().attr("albumId");
-			getPhotosFromAlbum(id);
-			$("#content img").click(function(event){
-               			event.preventDefault();
-               			PreviewImage($(this).attr('photoId'));
-	        	});
-
-			$("#navigationMenu li a[albumId=\""+id+"\"].hoverMenu").remove();
-			$("#navigationMenu li a[albumId="+id+"]").removeClass().addClass("selectedMenu");
-		});
-	});
-	
-	$('#navigationMenu li').hover(function(){
-	
-		$(this).find('.hoverMenu').stop().animate({marginTop:'0px'},200);
-
-	},
-	
-	function(){
-	
-		$(this).find('.hoverMenu').stop().animate({marginTop:'-25px'},200);
-
-	});
-	$("#content").ready(function(){
-		$("#content img").click(function(event){
-			event.preventDefault();
-			PreviewImage($(this).attr('photoId'));
-		});
 	});
 }
 
 function dispPublicPhotos(){
-	$.getJSON("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=acf8e0e35f7d224d8f071714e46a851d&user_id=106549958%40N08&per_page=20&format=json&nojsoncallback=1",
-        function(data){
-			$("#navigationMenu").empty();
-			$("#SetTitle").text("Dernières photos");
-			$.each(data.photos.photo,function( index, photo) {
-				$("#content").append("<img height='333px' photoId='"+photo.id+"' src='" + "http://c2.staticflickr.com/" + photo.farm + "/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg'/>");
-			});
-			initializeUI();
-        });	
+	$.getJSON("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=acf8e0e35f7d224d8f071714e46a851d&user_id=106549958%40N08&per_page=10&format=json&nojsoncallback=1", function(data){
+		$("#navigationMenu").empty();
+		$("#SetTitle").text("Dernières photos");
+		$.each(data.photos.photo,function( index, photo) {
+			$("#content").append("<div class='imgContainer'><img photoId='"+photo.id+"' src='"+constructPhotoUrl(photo.id,photo.farm,photo.server,photo.secret)+"'/></div>");
+		});
+		repaintMenu();
+	});
 }
 
 PreviewImage = function(photoId) {
